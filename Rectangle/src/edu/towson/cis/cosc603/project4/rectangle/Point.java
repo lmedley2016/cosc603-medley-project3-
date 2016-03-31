a7fec5f39bd21e20ef25bc208506dd3c8942d0a1
lmedley2016 <lmedle1@students.towson.edu>
2016-03-30 21:24:29 -0400

    Task 3 - A Little More Advanced - Rectangle.  This task was to find a bug that caused JUnit test to fail.  Turns out that the Point object as defined by Point(Double x, Double y) was set incorrectly with x value pointing to y, i.e., this.x = y.  The error was corrected to be this.x = x.  The JUnit test was run again successfully after the bug was fixed. Used Code Smells to refactor the code and one of the smells was Feature Envy.  Methods getArea and getDiagonal was moved to the Point class so the Rectangle class would only have to call these methods instead of them being in the Rectangle class.  This way the code was separated and easier to see, so the bug could be easily discovered.

---
+++ Rectangle/src/edu/towson/cis/cosc603/project4/rectangle/Point.java
@@ -0,0 +1,40 @@
+package edu.towson.cis.cosc603.project4.rectangle;
+
+/**
+ * The Point Class.
+ */
+public class Point {
+	
+	/** x and y coordinates. */
+	public Double x, y;
+	
+	/**
+	 * Instantiates a new point.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 */
+	Point(Double x, Double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Gets the area.
+	 * @param p2 Point p2 to calculate the area
+	 * @return  the area
+	 */
+	public Double getArea(Point p2) {
+		return Math.abs((p2.x - this.x) * (p2.y - this.y));
+	}
+
+	/**
+	 * Gets the diagonal.
+	 * @param p2 Point p2 to calculate the area
+	 * @return  the diagonal
+	 */
+	public Double getDiagonal(Point p2) {
+		return Math.sqrt(Math.pow((p2.x - this.x), 2)
+				+ Math.pow((p2.y - this.y), 2));
+	}
+}
