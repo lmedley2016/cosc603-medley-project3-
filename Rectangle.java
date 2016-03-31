62a22d05961e03347861eca93c95afba71fbf6b1
lmedley2016 <lmedle1@students.towson.edu>
2016-03-30 23:02:40 -0400

    Added files via upload

---
+++ Rectangle.java
@@ -0,0 +1,40 @@
+package edu.towson.cis.cosc603.project4.rectangle;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Rectangle.
+ */
+public class Rectangle {
+	
+	/** The p2. */
+	private Point p1, p2;
+	
+	/**
+	 * Instantiates a new rectangle.
+	 *
+	 * @param p1 the p1
+	 * @param p2 the p2
+	 */
+	Rectangle(Point p1, Point p2) {
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+	
+	/**
+	 * Gets the area.
+	 *
+	 * @return the area
+	 */
+	public Double getArea() {
+		return p1.getArea(p2);
+	}
+	
+	/**
+	 * Gets the diagonal.
+	 *
+	 * @return the diagonal
+	 */
+	public Double getDiagonal() {
+		return p1.getDiagonal(p2);
+	}
+}
